055e72ce670beb7a2da962ed6946c1e0a40f8b7d
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-19 08:23:36 -0700

    Create script.py

---
+++ script.py
@@ -0,0 +1,33 @@
+class script(object):
+    START_TEXT = """ <b>Hai {}!!!.
+I am Simple Rename Bot!😌</b>
+<code>You can rename any Telegram Document, using this bot! With Permenant Thumbnail 💯.</code>
+<i>For More Details check Help 📜</i>
+"""
+
+    RENAME_403_ERR = "What Are You Doing? You are Banned"
+    UPGRADE_TEXT = "ITs A Free Servies"
+    DOWNLOAD_START = "Give Me Some Time..."
+    UPLOAD_START = "Starting to upload..."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @SDBotsz **"
+    SAVED_THUMB = "Thumbnail Saved ✅ This Is Permanent"
+    DEL_THUMB = "Thumbnail cleared succesfully!"
+    NO_THUMB = "No thumbnails found!"
+    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
+    CUSTOM_CAPTION_UL_FILE = " "
+    HELP_USER = """<b>Need Help 🤔</b>
+<b>For Rename File</b>
+   
+<b>1.</b>Send me any Telegram File.
+<b>2.</b> Choose appropriate option.
+<b>For Thumbnail</b>
+<b>1.</b> Sent A Photo To Set As Thumnail.
+<b>2.</b> /showthumb To See Your Current Thumbnail.
+<b>3.</b> /delthumb To Delete Your Current Thumbnail.
+"""
+    ABOUT_ME = """ <b>Creator:</b> <a href="https://t.me/Darkridersslk">SADEW</a>
+<b>Credits:</b> Dan for his awesome pyrogram library 🔥
+<b>Library:</b> <a href="https://docs.pyrogram.org/">Pyrogram 1.1.13</a>
+<b>Updates:</b> <a href="https://t.me/SDBOTs_inifinity">SDBOTs</a>
+<b>Server:</b> <a href="https://heroku.com">Heroku</a>
+<b><u>NB:</u></b> EveryOne Can Use Me.So Don't Worry🤗"""
